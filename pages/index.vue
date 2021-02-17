3e56ef54fb245cc05c5f558261c16beab3eaa6e9
ismailreformed <ismailplusbd1994@gmail.com>
2021-02-17 11:43:35 +0600

    'updat'

--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,58 @@
 <template>
   <div class="">
     <v-card
-      class="mx-auto"
-      flat
+      class="mx-auto mb-10"
+      outlined
       max-width="1200"
     >
-    <v-card-title class="primary white--text">
-      <h3 class=" text-center">Vote Your Favourite Candidates</h3>
-    </v-card-title>
-    <v-card-text class="pa-2">
-      <div class="text-center">
-        <h3 class="unique-text">
-          <strong>
-            <v-chip class="warning" small>
-              Note
-            </v-chip>
-            You Can Votes A section Candidates only 
-            <v-chip class="primary" small>Once</v-chip>
-            !!
-            <br> 
-            And You Can votes by Click on 
-            <v-chip class="success" small>Yes</v-chip>
-             and 
-            <v-chip class="red white--text" small>No</v-chip>
-             Button
-          </strong>
-        </h3>
-      </div>    
-    </v-card-text>
+      <v-card-title class="primary white--text">
+        <h3 class=" text-center">Vote Your Favourite Candidates</h3>
+      </v-card-title>
+      <v-card-text class="pa-2">
+        <div class="text-center">
+          <h3 class="unique-text">
+            <strong>
+              <v-chip class="warning" small>
+                Note
+              </v-chip>
+              You Can Votes A section Candidates only 
+              <v-chip class="primary" small>Once</v-chip>
+              !!
+              <br> 
+              And You Can votes by Click on 
+              <v-chip class="success" small>Yes</v-chip>
+              and 
+              <v-chip class="red white--text" small>No</v-chip>
+              Button
+            </strong>
+          </h3>
+        </div>    
+      </v-card-text>
+    </v-card>
 
-    <v-card-text>
-       <v-data-table
-        :headers="headers"
-        :items="candidates"
-        class="elevation-1"
+    <v-card 
+      v-for="(position, i) in positions" 
+      :key="i" 
+      class="mx-auto mb-10"
+      outlined
+      max-width="1200" 
       >
-        <template v-slot:top>
-          <v-toolbar flat class="grey lighten-3">
-            <v-dialog v-model="votingDialog" max-width="300px">
-              <v-card>
-                <v-card-title class="headline"
-                  >Are you sure you want to vote this candidate?</v-card-title
-                >
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="red darken-1" text 
-                    >Yes
-                  </v-btn>
-                  <v-btn color="blue darken-1" text 
-                    >No
-                  </v-btn>
-                  <v-spacer></v-spacer>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </v-toolbar>
-        </template>
+      <v-card-title class="grey lighten-3">
+        Cast your vote for a 
+        <v-chip class="white--text ml-3" color="primary" label>{{position.name}}</v-chip>
+      </v-card-title>
 
+      <v-data-table
+        :headers="headers"
+        :items="position.candidates"
+        :disable-pagination="true"
+        hide-default-footer
+      >
         <template v-slot:item.actions="{ item }">
           <v-btn small color="success" class="mr-2">
             Yes
           </v-btn>
-          <v-btn color="red" small>
+          <v-btn color="red" class="white--text" small>
             No
           </v-btn>
         </template>
@@ -72,9 +63,25 @@
           </v-btn>
         </template>
       </v-data-table>
-    </v-card-text>
-      
     </v-card>
+    
+    <v-dialog v-model="votingDialog" max-width="300px">
+      <v-card>
+        <v-card-title class="headline"
+          >Are you sure you want to vote this candidate?</v-card-title
+        >
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="red darken-1" text 
+            >Yes
+          </v-btn>
+          <v-btn color="blue darken-1" text 
+            >No
+          </v-btn>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -84,9 +91,9 @@ import User from "@/static/helpers/User.js";
     data() {
       return {
         votingDialog: false,
-        candidates: [],
+        positions: [],
         headers: [
-          { text: "Icon", value: "team" },
+          { text: "Icon", value: "iocn" },
           {
             text: "Candidate Name",
             align: "start",
@@ -102,6 +109,23 @@ import User from "@/static/helpers/User.js";
     mounted(){
       if (!User.loggedIn()) {
 		    this.$router.push('/login')
+      }
+    },
+
+    created() {
+      this.initialize();
+    },
+
+    methods: {
+      initialize() {
+        this.$axios
+          .get("position")
+          .then(response => {
+            this.positions = response.data;
+          })
+          .catch(error => {
+            this.$toast.error(error.response.data.message);
+          });
       }
     }
   }
